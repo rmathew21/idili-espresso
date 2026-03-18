ec64e653ce90faf95772ea9fab352540d826c047
rob <robmathew21@gmail.com>
2026-03-18 16:43:13 -0500

    created src/components with empty files, added historyFallback to vite.config, added theme: font+colors to tailwind.config, added meta tags to index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Jost', 'sans-serif'],
+        display: ['"Playfair Display"', 'serif'],
+      },
+      colors: {
+        cream: '#FAF6EF',
+        espresso: '#2C1A0E',
+        mocha: '#6B3F1F',
+        gold: '#C9A96E',
+        'gold-light': '#E8D5B0',
+        sage: '#8A9E7A',
+      },
+    },
   },
   plugins: [],
 }
