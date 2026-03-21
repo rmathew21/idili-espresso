86011785bb02b8b986b79681970b7e13b1fc9c30
rob <robmathew21@gmail.com>
2026-03-21 00:25:54 -0500

    added src/pages with empty Home, Locations, Menu, and NotFound pages, started Home and added it to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
+    <div className="min-h-screen flex flex-col">
+    <Navbar />
+    <main className="flex-1">
+    <Routes>
+    <Route index element={<Home />} />
+    </Routes>
+    </main>
         <Footer />
-      </div>
+        </div>
+      
     </BrowserRouter>
-  )
+  );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const HERO_IMAGE = 'https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=1800&q=85'
+const COFFEE_IMAGE = 'https://images.unsplash.com/photo-1509042239860-f550ce710b93?w=900&q=80'
+const FOOD_IMAGE = 'https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?w=900&q=80'
+
+export default function Home() {
+    return (
+        <>
+        <h1>Home</h1>
+        </>
+    )
+}
